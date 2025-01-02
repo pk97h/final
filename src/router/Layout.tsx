4230af31d617fce332aab9b186d4f73e932c41d7
pk <yellowdayo@naver.com>
2025-01-02 12:24:45 +0900

    네비게이션 바 구현

--- src/router/Layout.tsx
+++ src/router/Layout.tsx
@@ -1,18 +1,45 @@
 import { Link, Outlet } from "react-router-dom";
 
 const Layout = () => {
+  const user = true;
+
   return (
     <>
-      <header className="flex space-x-4">
-        <Link to={"/"}>메인</Link>
-        <Link to={"/feeds/:id"}>상세</Link>
-        <Link to={"/feeds/create"}>추가</Link>
-        <Link to={"/feeds/update/:id"}>수정</Link>
-        <Link to={"/mypage"}>마이</Link>
-        <Link to={"/signup"}>회원가입</Link>
-        <Link to={"/login"}>로그인</Link>
-      </header>
-      <Outlet />
+      {user ? (
+        <>
+          <header>
+            <Link to={"/"}>
+              <img src="public/logoipsum-289.svg" />
+            </Link>
+            <div className="naivation-bar-link-wrapper">
+              <Link to={"/login"} className="navigation-bar-link">
+                로그인
+              </Link>
+              <Link to={"/signup"} className="navigation-bar-link">
+                회원가입
+              </Link>
+            </div>
+          </header>
+          <Outlet />
+        </>
+      ) : (
+        <>
+          <header>
+            <Link to={"/"}>
+              <img src="public/logoipsum-289.svg" />
+            </Link>
+            <div className="naivation-bar-link-wrapper">
+              <Link to={"/mypage"} className="navigation-bar-link">
+                마이 페이지
+              </Link>
+              <Link to={"/"} className="navigation-bar-link">
+                로그아웃
+              </Link>
+            </div>
+          </header>
+          <Outlet />
+        </>
+      )}
     </>
   );
 };
