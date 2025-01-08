f529bb8085bb93733983e1f157e2bb4b356922f9
pk <yellowdayo@naver.com>
2025-01-08 13:54:43 +0900

    home UI 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,28 @@
-const Home = () => {
+import { Link } from "react-router-dom";
+import Feed from "../components/Feed";
 
-  return <div>메인 페이지</div>;
+const Home = () => {
+  return (
+    <>
+      <div className="max-w-screen-xl min-h-[90vh] mx-auto px-10 my-10">
+        <div className="flex justify-between items-center mb-4">
+          <div className="text-2xl font-bold">글 목록</div>
+          <Link
+            to={"/feeds/create"}
+            className="bg-blue-500 text-white px-4 py-2 rounded-md"
+          >
+            글쓰기
+          </Link>
+        </div>
+        <div className="flex flex-col gap-4">
+          <Feed />
+          <Feed />
+          <Feed />
+          <Feed />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
